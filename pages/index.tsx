4c8a35f9e1e1ab2655f2751a8152febf377b9a33
ryota-murakami <dojce1048@gmail.com>
2020-07-30 17:18:06 +0900

    update description

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,10 +22,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          Laststance is individual open source software organization for place personal activity. {' '}
+          <a href="https://github.com/laststance">github.com/laststance</a>
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
