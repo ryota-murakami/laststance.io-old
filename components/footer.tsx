48ef044e5d5f41f7a6e9cdeaf8a2b057d88c5e99
ryota-murakami <dojce1048@gmail.com>
2020-11-13 01:26:47 +0900

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
+    <footer className="border-t border-accent-2">
       <Container>
-        <div className="pt-28 pb-8 flex flex-col lg:flex-row items-center">
-          <div className="flex flex-col lg:flex-row items-center lg:pl-4 lg:w-1/2">
-            <div>Copyright © 2020 Laststance.io All rights reserved.</div>
+        <div className="py-8 flex flex-col lg:flex-row items-center">
+          <div className="flex flex-row lg:pl-4 lg:w-1/2">
+            <div className="self-start">
+              Copyright © 2020 Laststance.io All rights reserved.
+            </div>
           </div>
         </div>
       </Container>
