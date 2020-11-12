fb72ab7abb896760615f38d7ebdd2bb41d90746f
ryota-murakami <dojce1048@gmail.com>
2020-11-13 01:16:28 +0900

    ts

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,4 +1,4 @@
-export default function Intro() {
+const Intro: React.FC = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
@@ -10,3 +10,5 @@ export default function Intro() {
     </section>
   )
 }
+
+export default Intro
