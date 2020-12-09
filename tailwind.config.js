b4e4b1508ae5bb7eed43b94911011ce1e5337c85
ryota-murakami <dojce1048@gmail.com>
2020-12-10 02:54:25 +0900

    add default breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@ module.exports = {
   purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
   darkMode: false,
   theme: {
+    theme: {
+      screens: {
+        sm: '640px',
+        // => @media (min-width: 640px) { ... }
+
+        md: '768px',
+        // => @media (min-width: 768px) { ... }
+
+        lg: '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        xl: '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+    },
     extend: {
       colors: {
         'accent-1': '#FAFAFA',
