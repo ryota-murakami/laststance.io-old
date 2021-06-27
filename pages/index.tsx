8ab3b9e9bf41d0a54be69aa3d10304417c5ff730
ryota-murakami <dojce1048@gmail.com>
2021-06-27 18:03:55 +0900

    text

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,13 @@ const Index: React.FC = () => {
       <div className="w-full mx-auto py-16">
         <div className="text-center">
           <h2 className="text-4xl font-semibold text-indigo-600 tracking-wide uppercase">
-            What is the Web Browser?
+            Web Engineering is my only option
           </h2>
           <p className="mt-10 text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-extrabold text-gray-900">
             Laststance.io
           </p>
           <p className="max-w-xl mt-10 mx-auto leading-none text-2xl sm:text-5xl md:text-5xl lg:text-5xl text-gray-500">
-            Laststance.io is independent organization for OSS activity.
+            Laststance.io is independent OSS Doujin circle.
           </p>
         </div>
       </div>
