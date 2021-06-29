4c751e4d8bfed3bbb89db396cb5ddc8d2227b1eb
ryota-murakami <dojce1048@gmail.com>
2021-06-29 17:15:09 +0900

    ajust footer height

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer: React.FC = () => {
   return (
     <footer className="border-t border-accent-2 w-screen h-16">
-      <div className="py-8 flex flex-col lg:flex-row items-center container m-auto">
+      <div className="my-8 flex flex-col lg:flex-row items-center container m-auto">
         <div className="flex flex-row lg:pl-4 lg:w-1/2">
           <div className="self-start">
             Copyright © {new Date().getFullYear()} Laststance.io All rights
