b82682503c6bb664e5e633d4b4c2df7d75a5bb39
ryota-murakami <dojce1048@gmail.com>
2021-07-04 00:25:34 +0900

    tmp

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,7 @@ const About: React.FC = () => {
       <h1 className="py-6 text-4xl text-center">About</h1>
       <section className="text-xl">
         <div>
-          Laststance.io is place for sole OpenSourceSoftware activity by{' '}
+          Laststance.io is organization for Open Source Software activity by{' '}
           <a
             className="hover:text-gray-400 text-gray-600"
             href="https://ryota-murakami.github.io/"
@@ -16,6 +16,12 @@ const About: React.FC = () => {
             Ryota Murakami
           </a>
           .
+        </div>
+
+        <div className="mt4">
+          Yes I created organization alone, that little bit odd.
+          <br />
+          Initial motivation is I want to put prefix @laststance/foopkg
         </div>
 
         <div className="mt-4">
@@ -32,6 +38,13 @@ const About: React.FC = () => {
             Join core of OSS that crucial thing future of web and health of web.
           </li>
         </ul>
+
+        <div className="mt-4">
+          We prefer show example of any mainstream React/JavaScript technology
+          for developers who trying to build a Real World App like Youtube,
+          Instagram, Twitter, Facebook etc. We prefer building with fully OSS
+          technology stack rathar than PaSS like Firebase.
+        </div>
       </section>
     </div>
   )
