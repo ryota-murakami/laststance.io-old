a071759a935b65dcb7ddf7b70d10feb3ef373102
ryota-murakami <dojce1048@gmail.com>
2021-07-25 02:13:49 +0900

    update blog text style

--- components/pages/posts/PostTitle.tsx
+++ components/pages/posts/PostTitle.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PostTitle: React.FC = ({ children }) => {
   return (
-    <h1 className="text-5xl font-bold mb-8 mt-8 text-center tracking-tighter leading-tight md:pl-8 md-pr-4 md:leading-none md:text-left">
+    <h1 className="text-4xl md:text-5xl leading-tight mb-8 mt-8 text-center md:pl-8 md-pr-4 md:leading-none md:text-left">
       {children}
     </h1>
   )
--- components/pages/posts/[id]/PostHeader.tsx
+++ components/pages/posts/[id]/PostHeader.tsx
@@ -14,7 +14,7 @@ interface Props {
 const PostHeader: React.FC<Props> = ({ title, date, author }) => (
   <>
     <PostTitle>{title}</PostTitle>
-    <div className="hidden md:block md:mb-12">
+    <div className="hidden md:block">
       <Avatar name={author.name} picture={author.picture} />
     </div>
     <div className="max-w-2xl mx-auto">
