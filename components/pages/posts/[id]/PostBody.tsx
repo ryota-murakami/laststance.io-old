e45b5a51aecc4b09d186dc303e8c7b799591b9e0
ryota-murakami <dojce1048@gmail.com>
2021-07-27 05:59:48 +0900

    prose

--- components/pages/posts/[id]/PostBody.tsx
+++ components/pages/posts/[id]/PostBody.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import markdownStyles from './MarkdownStyles.module.css'
 import BlockContent from '@sanity/block-content-to-react'
 import { Post } from '../../../../types'
 
@@ -10,7 +9,7 @@ interface Props {
 export const PostBody: React.FC<Props> = ({ content }) => {
   return (
     <div className="max-w-2xl mx-auto">
-      <BlockContent blocks={content} className={markdownStyles.markdown} />
+      <BlockContent blocks={content} className="prose" />
     </div>
   )
 }
