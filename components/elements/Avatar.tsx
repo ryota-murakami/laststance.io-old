8a731306a81eee29323936ab5b9c9d313b3f86d9
ryota-murakami <dojce1048@gmail.com>
2021-08-03 03:48:34 +0900

    ratio

--- components/elements/Avatar.tsx
+++ components/elements/Avatar.tsx
@@ -6,10 +6,16 @@ interface Props {
   picture: Author['picture']
 }
 
+// @TODO fix aspect retio
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const Avatar: React.FC<Props> = ({ name, picture }) => {
   return (
     <div className="flex items-center">
-      <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <img
+        src="https://cdn.sanity.io/images/8qfjmpmv/production/0453a311ba3f903590b5c49e50cfce6e9d3110d6-1700x2267.jpg?rect=0,283,1700,1700&w=80&h=80&fit=crop"
+        className="w-12 h-12 rounded-full mr-4"
+        alt={name}
+      />
       <div className="text-xl font-bold">{name}</div>
     </div>
   )
