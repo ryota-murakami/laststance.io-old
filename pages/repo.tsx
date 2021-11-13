2fba26757d8d46f21f460c79918fb1f7a45723c9
ryota-murakami <dojce1048@gmail.com>
2021-11-13 22:41:00 +0900

    add content

--- pages/repo.tsx
+++ pages/repo.tsx
@@ -2,48 +2,57 @@ import React, { memo } from 'react'
 
 import RepoCard from '../components/elements/RepoCard'
 
-const Repo: React.FC = memo(() => {
-  return (
-    <section className="container flex flex-col items-center m-auto h-screen">
-      <RepoCard
-        name="vite-react-ts-extended"
-        url="https://github.com/laststance/vite-react-ts-extended"
-        intro="Vite's react-ts templete + eslint, jest, tailwindcss."
-      >
-        This is the official Vite template(npm init vite@latest{' '}
-        <span>(npm init vite@latest myapp -- --template react-ts)</span> with
-        the following additional set up.
-      </RepoCard>
-      <RepoCard
-        name="vite-react-ts-extended"
-        url="https://github.com/laststance/vite-react-ts-extended"
-        intro="Vite's react-ts templete + eslint, jest, tailwindcss."
-      >
-        This is the official Vite template(npm init vite@latest{' '}
-        <span>(npm init vite@latest myapp -- --template react-ts)</span> with
-        the following additional set up.
-      </RepoCard>
-      <RepoCard
-        name="vite-react-ts-extended"
-        url="https://github.com/laststance/vite-react-ts-extended"
-        intro="Vite's react-ts templete + eslint, jest, tailwindcss."
-      >
-        This is the official Vite template(npm init vite@latest{' '}
-        <span>(npm init vite@latest myapp -- --template react-ts)</span> with
-        the following additional set up.
-      </RepoCard>
-      <RepoCard
-        name="vite-react-ts-extended"
-        url="https://github.com/laststance/vite-react-ts-extended"
-        intro="Vite's react-ts templete + eslint, jest, tailwindcss."
-      >
-        This is the official Vite template(npm init vite@latest{' '}
-        <span>(npm init vite@latest myapp -- --template react-ts)</span> with
-        the following additional set up.
-      </RepoCard>
-    </section>
-  )
-})
+const Repo: React.FC = memo(
+  () => {
+    return (
+      <section className="container flex flex-col items-center m-auto h-screen">
+        <RepoCard
+          name="vite-react-ts-extended"
+          url="https://github.com/laststance/vite-react-ts-extended"
+          intro="Vite's react-ts templete + eslint, jest, tailwindcss."
+        >
+          This is the official Vite template(npm init vite@latest{' '}
+          <span>(npm init vite@latest myapp -- --template react-ts)</span> with
+          the following additional set up.
+        </RepoCard>
+        <RepoCard
+          name="today-i-learned"
+          url="https://github.com/laststance/today-i-learned"
+          intro="Micro Blog System for personal use (β) 🚧"
+        >
+          The ultimate goal for this project is release it as a personal blog
+          starter kit for React developers like Minimal Wordpress. I'm
+          developping essential feature while my spare time, currenty I planning
+          publish v1 2023 early.
+        </RepoCard>
+        <RepoCard
+          name="create-react-app-typescript-todo-example-2021"
+          url="https://github.com/laststance/create-react-app-typescript-todo-example-2021"
+          intro="🚀 Create React App TypeScript Todo Example 2021"
+        >
+          Keep updating and publish{' '}
+          <a
+            href="https://todomvc.com/"
+            target="_blank"
+            className="no-underline hover:underline font-medium text-gray-700"
+          >
+            TODO MVC stuff
+          </a>{' '}
+          with a modern code style React.
+        </RepoCard>
+        <RepoCard
+          name="use-app-state"
+          url="https://github.com/laststance/use-app-state"
+          intro="🌏 useAppState() hook. that global version of setState() built on useContext."
+        >
+          I wanted just setState() but can use across the another components for
+          prototyping.
+        </RepoCard>
+      </section>
+    )
+  },
+  () => true
+)
 Repo.displayName = 'Repo'
 
 export default Repo
