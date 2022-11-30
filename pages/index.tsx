fabbf5a69d286ef3b89bb34fab6fc090bcbddd7d
ryota-murakami <dojce1048@gmail.com>
2022-11-30 17:54:06 +0900

    Straightforward Web Technology for Better World

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,7 @@ const Index: React.FC = memo(
     return (
       <main className="main relative bottom-20 flex flex-col justify-center space-y-10 sp:bottom-5">
         <h2 className="text-center text-4xl font-semibold text-indigo-600">
-          UI<span className="mx-0.5">×</span>
-          Data<span className="mx-0.5">×</span>
-          Web<span className="mx-0.5">×</span>
-          Code<span className="mx-1"></span> makes our Life Infrastructure.
+          Straightforward Web Technology for Better World.
         </h2>
         <h1 className="overflow-hidden overflow-clip text-center text-5xl font-bold text-gray-900 md:text-8xl lg:text-9xl">
           Laststance.io
